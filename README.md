43b9287c6855d9a145eff7a582027bfaa1aaea11
Joel Wasserman <joel.wasserman8@gmail.com>
2016-07-05 14:44:11 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#React Native Toast Tutorial
+
+This is the code for the react native android module toast tutorial from facebook
+
+https://facebook.github.io/react-native/docs/native-modules-android.html 
+
+#LICENSE
+
+MIT
---
+++ android/app/src/main/java/com/nativetoastmodule/toastmodule/ToastModulePackage.java
@@ -0,0 +1,40 @@
+package com.nativetoastmodule.toastmodule;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+/**
+ * Created by joelwasserman on 7/5/16.
+ */
+public class ToastModulePackage implements ReactPackage {
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new ToastModule(reactContext));
+
+        return modules;
+    }
+
+}
